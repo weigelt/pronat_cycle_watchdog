5075f27c80f565e8e556b6bed5a041a260a3d56e
Sebastian Weigelt <weigelt@kit.edu>
2020-10-13 19:17:11 +0200

    basic impl of watchdog

--- src/main/java/edu/kit/ipd/parse/cycleWD/CycleWatchdog.java
+++ src/main/java/edu/kit/ipd/parse/cycleWD/CycleWatchdog.java
@@ -4,9 +4,8 @@ import edu.kit.ipd.parse.luna.AbstractLuna;
 import edu.kit.ipd.parse.luna.agent.AbstractAgent;
 import edu.kit.ipd.parse.luna.agent.AbstractWatchdog;
 import edu.kit.ipd.parse.luna.tools.ConfigManager;
+import org.apache.commons.collections4.queue.CircularFifoQueue;
 import org.kohsuke.MetaInfServices;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
@@ -19,13 +18,14 @@ import java.util.Properties;
 public class CycleWatchdog extends AbstractWatchdog {
 
 	private static final String ID = "cycleWatchdog";
-	private static final Logger logger = LoggerFactory.getLogger(CycleWatchdog.class);
 	private static final Properties wdProps = ConfigManager.getConfiguration(CycleWatchdog.class);
 	private static final Properties lunaProps = ConfigManager.getConfiguration(AbstractLuna.class);
 
 	private static final String PROP_TERM_SIGNAL_TYPE = "TERM_SIGNAL_TYPE";
+	private static final String PROP_MONITORED_GRAPHS = "MONITORED_GRAPHS";
 
 	private String termSignalType;
+	private CircularFifoQueue<Long> graph_queue;
 
 	public CycleWatchdog() {
 		setId(ID);
@@ -37,6 +37,7 @@ public class CycleWatchdog extends AbstractWatchdog {
 	@Override
 	public void init() {
 		termSignalType = lunaProps.getProperty(PROP_TERM_SIGNAL_TYPE);
+		graph_queue = new CircularFifoQueue<>(Integer.parseInt(wdProps.getProperty(PROP_MONITORED_GRAPHS)));
 	}
 
 	/**
@@ -44,6 +45,11 @@ public class CycleWatchdog extends AbstractWatchdog {
 	 */
 	@Override
 	protected void exec() {
-
+		long currHash = graph.hashCode();
+		if (graph_queue.contains(currHash)) {
+			graph.createNodeType(termSignalType);
+		} else {
+			graph_queue.add(currHash);
+		}
 	}
 }
